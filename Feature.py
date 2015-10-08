b41c7ca559685962a80cba31857390a695461795
along528 <along528@gmail.com>
2015-10-08 14:45:48 -0400

    Updated Feature with all objects

--- Feature.py
+++ Feature.py
@@ -6,6 +6,8 @@ class Feature:
     def __init__(self,featureType):
         self.featureType = featureType
 	self.logicalAND = False
+	self.onAllLeptons = False
+	self.requiresSum = False
 	self.cuts = []
 	self.cutString = ""
 	self.Configure()
@@ -35,110 +37,69 @@ class Feature:
 	cutString = ""
   	if self.featureType == "BJetVeto":
 		cutString = "btagEff85 <SIGN> <THRESHOLD>"  
+		self.onAllLeptons = False
   	elif self.featureType == "JetVeto":
 		cutString = "@jets_btagged.size() <SIGN> <THRESHOLD> " 
+		self.onAllLeptons = False
   	elif self.featureType == "PtInc":
-		#MeV
-		cutString = "Sum$(lep_momentum.Pt() <SIGN> <THRESHOLD>) >= 3"
+		#cutString = "Sum$(lep_pt <SIGN> <THRESHOLD>) >= 3"
+		cutString = "lep_pt <SIGN> <THRESHOLD>"
+		self.onAllLeptons = True
+  	elif self.featureType == "Phi":
+		#cutString = "Sum$(lep_phi <SIGN> <THRESHOLD>) >= 3"
+		cutString = "lep_phi <SIGN> <THRESHOLD>"
+		self.onAllLeptons = True
+  	elif self.featureType == "Eta":
+		#cutString = "Sum$(lep_eta <SIGN> <THRESHOLD>) >= 3"
+		cutString = "lep_eta <SIGN> <THRESHOLD>"
+		self.onAllLeptons = True
+  	elif self.featureType == "Charge":
+		#cutString = "Sum$(lep_charge <SIGN> <THRESHOLD>) >= 3"
+		cutString = "lep_charge <SIGN> <THRESHOLD>"
+		self.onAllLeptons = True
+  	elif self.featureType == "MassElEl":
+		#cutString = "Sum$(masses_ElEl <SIGN> <THRESHOLD>) >= 3"
+		cutString = "masses_ElEl <SIGN> <THRESHOLD>"
+		self.onAllLeptons = True
+  	elif self.featureType == "MassElMu":
+		#cutString = "Sum$(masses_ElMu <SIGN> <THRESHOLD>) >= 3"
+		cutString = "masses_ElMu <SIGN> <THRESHOLD>"
+		self.onAllLeptons = True
+  	elif self.featureType == "MassMuMu":
+		#cutString = "Sum$(masses_MuMu <SIGN> <THRESHOLD>) >= 3"
+		cutString = "masses_MuMu <SIGN> <THRESHOLD>"
+		self.onAllLeptons = True
+  	elif self.featureType == "MassSFOS":
+		#cutString = "Sum$(masses_SFOS <SIGN> <THRESHOLD>) >= 3"
+		cutString = "masses_SFOS <SIGN> <THRESHOLD>"
+		self.onAllLeptons = True
   	elif self.featureType == "MET":
-		#MeV
 		cutString = "MET_final_Et <SIGN> <THRESHOLD>"
+		self.onAllLeptons = False
+  	elif self.featureType == "METPhi":
+		cutString = "MET_final_phi <SIGN> <THRESHOLD>"
+		self.onAllLeptons = False
+  	elif self.featureType == "METSumEt":
+		cutString = "MET_final_sumEt <SIGN> <THRESHOLD>"
+		self.onAllLeptons = False
   	elif self.featureType == "NSFOS":
 		cutString = "nSFOS <SIGN> <THRESHOLD>"
+		self.onAllLeptons = False
   	elif self.featureType == "NMu":
-		cutString = "Sum$(lep_isMuon) <SIGN> <THRESHOLD>"
 		#in 0 SFOS you have eem and mme (NMUONS == 1 or 2)
 		#in 1 SFOS you also have eem and mme (NMUONS == 1 or 2)
 		#in 2 SFOS you have eee and mmm (NMUONS == 0 or 3)
+		cutString = "Sum$(lep_isMuon) <SIGN> <THRESHOLD>"
+		self.onAllLeptons = False
   	elif self.featureType == "Mt":
 		cutString ="allLep_mt <SIGN> <THRESHOLD>"
-		#self.parameters["Thresh"] = range(0,60*GeV,10*GeV)
-		#self.parametersInclusive["Thresh"] = 0
+		self.onAllLeptons = False
   	elif self.featureType == "DPhi":
-		cutString = "TMath::Abs(allLep_deltaPhiMET) <SIGN> <THRESHOLD>"
+		cutString = "allLep_deltaPhiMET <SIGN> <THRESHOLD>"
+		self.onAllLeptons = False
 	else:
 		print "Cut",self.featureType,"Not understood!"
 		exit(2)
-	"""
-	#I need to think about how to do the Zveto stuff well, may want to take absoute value of difference with z mass
-  	elif self.featureType == "ZVeto":
-		#MeV
-		#cutString ="Sum$(masses_SFOS > 91187.6 - <ZWindowDown> && masses_SFOS < 91187.6 + <ZWindowUp> )==0"
-		cutString ="Sum$(masses_SFOS <SIGN> <THRESHOLD> )==0"
-  	#elif self.featureType == "ZVetoSymm":
-	#	cutString ="Sum$(masses_SFOS > 91187.6 - <ZWindow> && masses_SFOS < 91187.6 + <ZWindow> )==0"
-	#	self.parameters["ZWindow"] = [15*GeV] 
-	#	self.parametersInclusive["ZWindow"] = 0
-  	elif self.featureType == "ZVeto_ElEl":
-		#cutString = "((<1l2l>*(masses_ElEl[0] > 91187.6 - <ZWindow> && masses_ElEl[0] < 91187.6 + <ZWindow> ))+ (<1l3l>*(masses_ElEl[1] > 91187.6 - <ZWindow> && masses_ElEl[1] < 91187.6 + <ZWindow> ))+ (<2l3l>*(masses_ElEl[2] > 91187.6 - <ZWindow> && masses_ElEl[2] < 91187.6 + <ZWindow> )))==0"
-		cutString = "((<1l2l>*(masses_ElEl[0] > 91187.6 - <ZWindow> && masses_ElEl[0] < 91187.6 + <ZWindow> ))+ (<1l3l>*(masses_ElEl[1] > 91187.6 - <ZWindow> && masses_ElEl[1] < 91187.6 + <ZWindow> ))+ (<2l3l>*(masses_ElEl[2] > 91187.6 - <ZWindow> && masses_ElEl[2] < 91187.6 + <ZWindow> )))==0"
-		self.parameters["1l2l"] = [1]
-		self.parameters["1l3l"] = [1]
-		self.parameters["2l3l"] = [1]
-		self.parametersInclusive["1l2l"] = 1
-		self.parametersInclusive["1l3l"] = 1
-		self.parametersInclusive["2l3l"] = 1
-		self.parameters["ZWindow"] = [15*GeV] 
-		self.parametersInclusive["ZWindow"] = 0
-  	elif self.featureType == "ZVeto_MuMu":
-		cutString = "((<1l2l>*(masses_MuMu[0] > 91187.6 - <ZWindow> && masses_MuMu[0] < 91187.6 + <ZWindow> ))+ (<1l3l>*(masses_MuMu[1] > 91187.6 - <ZWindow> && masses_MuMu[1] < 91187.6 + <ZWindow> ))+ (<2l3l>*(masses_MuMu[2] > 91187.6 - <ZWindow> && masses_MuMu[2] < 91187.6 + <ZWindow> )))==0"
-		self.parameters["ZWindow"] = range(0,40*GeV,10*GeV)
-		self.parameters["ZWindow"] = [15*GeV] #range(0,40*GeV,10*GeV)
-		self.parametersInclusive["ZWindow"] = 0
-		self.parameters["1l2l"] = [1]
-		self.parameters["1l3l"] = [1]
-		self.parameters["2l3l"] = [1]
-		self.parametersInclusive["1l2l"] = 1
-		self.parametersInclusive["1l3l"] = 1
-		self.parametersInclusive["2l3l"] = 1
-  	elif self.featureType == "ZVeto_ElMu":
-		cutString = "((<1l2l>*(masses_ElMu[0] > 91187.6 - <ZWindow> && masses_ElMu[0] < 91187.6 + <ZWindow> ))+ (<1l3l>*(masses_ElMu[1] > 91187.6 - <ZWindow> && masses_ElMu[1] < 91187.6 + <ZWindow> ))+ (<2l3l>*(masses_ElMu[2] > 91187.6 - <ZWindow> && masses_ElMu[2] < 91187.6 + <ZWindow> )))==0"
-		self.parameters["ZWindow"] = [15*GeV] 
-		self.parametersInclusive["ZWindow"] = 0
-		self.parameters["1l2l"] = [1]
-		self.parameters["1l3l"] = [1]
-		self.parameters["2l3l"] = [1]
-		self.parametersInclusive["1l2l"] = 1
-		self.parametersInclusive["1l3l"] = 1
-		self.parametersInclusive["2l3l"] = 1
-
-  	elif self.featureType == "Mass_ElEl":
-		cutString = "(<All>*((<1l2l>*(masses_ElEl[0] < <Thresh> && masses_ElEl[0] >= 0 ))+ (<1l3l>*(masses_ElEl[1] < <Thresh> && masses_ElEl[1] >=0 ))+ (<2l3l>*(masses_ElEl[2] < <Thresh>  && masses_ElEl[2] >=0 ))))==0"
-		self.parameters["1l2l"] = [1]
-		self.parameters["1l3l"] = [1]
-		self.parameters["2l3l"] = [1]
-		self.parameters["All"] = [1]
-		self.parametersInclusive["1l2l"] = 1
-		self.parametersInclusive["1l3l"] = 1
-		self.parametersInclusive["2l3l"] = 1
-		self.parametersInclusive["All"] = 1
-		self.parameters["Thresh"] = [15*GeV] 
-		self.parametersInclusive["Thresh"] = 0
-  	elif self.featureType == "Mass_MuMu":
-		cutString = "(<All>*((<1l2l>*(masses_MuMu[0] < <Thresh> && masses_MuMu[0] >= 0 ))+ (<1l3l>*(masses_MuMu[1] < <Thresh> && masses_MuMu[1] >=0 ))+ (<2l3l>*(masses_MuMu[2] < <Thresh>  && masses_MuMu[2] >=0 ))))==0"
-		self.parameters["Thresh"] = [15*GeV] #range(0,40*GeV,10*GeV)
-		self.parametersInclusive["Thresh"] = 0
-		self.parameters["1l2l"] = [1]
-		self.parameters["1l3l"] = [1]
-		self.parameters["2l3l"] = [1]
-		self.parameters["All"] = [1]
-		self.parametersInclusive["1l2l"] = 1
-		self.parametersInclusive["1l3l"] = 1
-		self.parametersInclusive["2l3l"] = 1
-		self.parametersInclusive["All"] = 1
-  	elif self.featureType == "Mass_ElMu":
-		cutString = "(<All>*((<1l2l>*(masses_ElMu[0] < <Thresh> && masses_ElMu[0] >= 0 ))+ (<1l3l>*(masses_ElMu[1] < <Thresh> && masses_ElMu[1] >=0 ))+ (<2l3l>*(masses_ElMu[2] < <Thresh>  && masses_ElMu[2] >=0 ))))==0"
-		self.parameters["Thresh"] = [15*GeV] 
-		self.parametersInclusive["Thresh"] = 0
-		self.parameters["1l2l"] = [1]
-		self.parameters["1l3l"] = [1]
-		self.parameters["2l3l"] = [1]
-		self.parameters["All"] = [1]
-		self.parametersInclusive["1l2l"] = 1
-		self.parametersInclusive["1l3l"] = 1
-		self.parametersInclusive["2l3l"] = 1
-		self.parametersInclusive["All"] = 1
-	"""
 
 	self.cutString = ""
 	if threshold==None or sign==None: return
@@ -151,5 +112,7 @@ class Feature:
 	if len(self.cuts)>1:
 		self.cutString+= " %s " % ("&&" if self.logicalAND else "||")
 		self.cutString+= self.cuts[1]
+	if self.onAllLeptons:
+		self.cutString = "Sum$("+self.cutString+") >=3"
 
 
